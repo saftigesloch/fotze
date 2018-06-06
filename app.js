6a2898ad506a91f0d004f853d990909706793e3f
saftigesloch <39908256+saftigesloch@users.noreply.github.com>
2018-06-06 04:30:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
+var handler = require('express')();
+var server  = require('http').createServer(handler)
+var io = require('socket.io')(server);
 var fs = require('fs');
 
-app.listen(8080);
+server.listen(8080);
+console.log("Running on http-port 8080...");
 
-console.log("Fotze is Running on http-port 8080...");
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html', function(err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
 
-
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
+        res.writeHead(200);
+        res.end(data);
+    });
 }
 
-io.on('connection', function(socket){
-  console.log( new Date(), 'Schwanz connected');
-  socket.on('disconnect', function(){
-    console.log( new Date(),'Schwanz disconnected');
-  });
-});
+io.on('connection', function (socket) {
+    console.log("--# client connected:", new Date());
 
+    socket.on('device', function (msg) {
+        console.log("  # dev. --> " + msg);
+
+    });
+
+    socket.on('disconnect', function () {
+        console.log("--# client disconnected:", new Date());
+    });
+});
