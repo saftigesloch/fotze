bae0ccdc278a1f3d664e844908a64c94b1367798
saftigesloch <39908256+saftigesloch@users.noreply.github.com>
2018-06-17 18:36:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var server  = require('http').createServer(handler)
 var io = require('socket.io')(server);
 var fs = require('fs');
+var moment = require('moment');
+
 
 server.listen(8080);
+
+
+
+
 console.log("Running on http-port 8080...");
+console.log( moment().minute()); 
+
 
 function handler(req, res) {
     fs.readFile(__dirname + '/index.html', function(err, data) {
@@ -17,15 +25,31 @@ function handler(req, res) {
     });
 }
 
+
 io.on('connection', function (socket) {
-    console.log("--# client connected:", new Date());
 
-    socket.on('device', function (msg) {
-        console.log("  # dev. --> " + msg);
+        
+        console.log("--# client connected:", new Date());
+        io.emit('event message', "client connected: " + new Date());
+        io.emit('notify message', "user connected");
 
+        socket.on('device', function (msg) {
+            console.log("  # dev. --> " + msg);
+        });
+
+        socket.on('chat message', function (msg) {
+            console.log('message: ' + msg);
+            io.emit('chat message', msg);
+        });
+
+        socket.on('notify message', function (msg) {
+            console.log('notify: ' + msg);
+            io.emit('notify message', msg);
+        });
+
+        socket.on('disconnect', function () {
+            console.log("--# client disconnected:", new Date());
+            io.emit('event message', "client disconnected: " + new Date());
+            io.emit('notify message', "user disconnected");
+        });
     });
-
-    socket.on('disconnect', function () {
-        console.log("--# client disconnected:", new Date());
-    });
-});
