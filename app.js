05af18b0971a73a74a78d68b37c56cb38372f017
saftigesloch <39908256+saftigesloch@users.noreply.github.com>
2018-06-25 16:59:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,8 @@ var server  = require('http').createServer(handler)
 var io = require('socket.io')(server);
 var fs = require('fs');
 var moment = require('moment');
-
-
 server.listen(8080);
-
-
-
-
 console.log("Running on http-port 8080...");
-console.log( moment().minute()); 
-
 
 function handler(req, res) {
     fs.readFile(__dirname + '/index.html', function(err, data) {
@@ -26,35 +18,48 @@ function handler(req, res) {
 }
 
 
-io.on('connection', function (socket, username) {
+io.on('connection', function (socket) {
+
+            
+
+
+        console.log("--# client connected !26:", new Date());
+        io.emit('event message', " client connected !27: " + new Date());
+        
+        console.log('notify message', socket.username + " connected !29");
+        
 
         
-        console.log("--# client connected:", new Date());
-        io.emit('event message', "client connected: " + new Date());
-        io.emit('notify message', "user connected");
 
         socket.on('device', function (msg) {
-            console.log("  # dev. --> " + msg);
+            console.log(" !35 # dev. --> " + msg);
         });
 
         socket.on('cunt', function(username) {
             socket.username = username;
-        });
-    
+            
+            });
+
         socket.on('chat message', function (msg) {
-            console.log(socket.username + 'message: ' + msg);
+            console.log(socket.username + ' ->: ' + msg);
             io.emit('cunt', socket.username);
             io.emit('chat message', msg);
         });
 
         socket.on('notify message', function (msg) {
-            console.log('notify: ' + msg);
+            console.log('notify: !50' + msg);
             io.emit('notify message', msg);
         });
 
         socket.on('disconnect', function () {
-            console.log("--# client disconnected:", new Date());
+            console.log("--# client disconnected !54:", new Date());
             io.emit('event message', "client disconnected: " + new Date());
-            io.emit('notify message', "user disconnected");
+            io.emit('notify message', "User " + socket.username + " disconnected");
+            console.log('notify message', "User " + socket.username + " disconnected !58");
         });
     });
+
+
+
+
+// 06/25/18
